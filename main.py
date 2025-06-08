420f0fc5461ab40cb6019173fcc47ec805b13d47
Gabriela Panelli <patriciagpanelli@gmail.com>
2025-06-08 20:43:14 -0300

    conexion de practica con mysql

--- main.py
+++ main.py
@@ -8,15 +8,15 @@ csv_path = path.join(path.dirname(__file__), "files/ipc-chaco-historico.csv")
 excel_path =path.join(path.dirname(__file__), "files/Datos_de _servicio.xlsx")
 
 # Cargar y transformar
-#csv = DatasetCSV(csv_path)
-#csv.cargar_datos()
-#csv.mostrar_resumen()
+csv = DatasetCSV(csv_path)
+csv.cargar_datos()
+csv.mostrar_resumen()
 
 excel= DatasetExcel(excel_path)
 excel.cargar_datos()
 excel.mostrar_resumen()
 
 # guardar en base de datos
-#db = DataSaver("db/colecta.db")
+db = DataSaver()
 #db = db.guardar_dataframe(csv.datos, "ipc_chaco_historico")
-#db = db.guardar_dataframe(excel.datos, "Datos_de_servicio")
+db = db.guardar_dataframe(excel.datos, "datos")
